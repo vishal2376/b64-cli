be4a427a7c146541038dc8ac881e24e08ab55910
Vishal Singh <vishalsingh2376@gmail.com>
2023-12-26 18:29:45 +0530

    add desc and fix bug

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 use clap::Parser;
 
-const ABOUT: &str = "Simple Base64 Encoder and Decoder Utility";
+const ABOUT: &str = "Simple Utility to encode or decode string";
 
 #[derive(Parser)]
 #[command(author,version,about=ABOUT)]
 #[command(arg_required_else_help = true)]
 struct Cli {
-    #[arg(short, long)]
+    #[arg(short, long, required = false)]
+    /// Encode or Decode Text
     text: String,
 
-    #[arg(short, long, default_value = "false")]
+    #[arg(short, long)]
+    /// Decode Text
     decode: bool,
 }
 
 fn main() {
     let args = Cli::parse();
+    let content = args.text;
 
     if args.decode {
-        todo!("impl decoding string")
-    } else {
-        let content = args.text.as_bytes();
+        let binary_string = rbase64::decode(&content).unwrap();
 
-        let output = rbase64::encode(&content);
+        let output = String::from_utf8_lossy(&binary_string);
+        println!("Decoded String : {}", output);
+    } else {
+        let output = rbase64::encode(&content.as_bytes());
 
         println!("Encoded String : {}", output);
     }
